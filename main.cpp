32e140309d270d5973146ec40424338a42533f3a
YASIN MAOW <142474303+2468yas@users.noreply.github.com>
2025-04-10 17:16:09 -0700

    Update main.cpp
    Cleaned up the comments and added necessary spacing blocks

--- main.cpp
+++ main.cpp
@@ -24,27 +24,8 @@ void printVector(vector<int> v){
     cout << endl;
 }
 
-/* NOTE: I tried to write my own vector sorting function but I just couldn't get
-it to work. I don't quite understand what I was reading about how I have to set
-iterator types using a pointer. I'm going to admit defeat and use <algorithm>
-to sort this vector. I would love some feedback on what I'm doing wrong here.
 
-// Vector sorting function. "Makin' Medians EZ" since 2024.
-void sortVec(vector<int> &v) {  // Pass by reference to sort the input vector
-    int min;
-    auto minpos = v.begin();
-    for (int i = 0; i < v.size(); ++i) {
-        min = 201;
-        for (int j = i; j < v.size(); ++j) {
-            if (v.at(j) <= min) {
-                min = v.at(j);
-                *minpos = j;
-            }
-        }
-        v.erase(minpos);
-        v.insert(v.begin(), min);
-    }
-} */
+
 
 void barGraph(vector<int> v) {
     int count = 0;
@@ -56,10 +37,9 @@ void barGraph(vector<int> v) {
     }
     for (unsigned int i = v.at(0); i <= v.at(v.size() - 1); ++i) {
         for (int j : v) if (j == i) count++;
-        // Brevity is the soul of wit.
-        // (AKA *I* know what this is doing...)
+    
+    
         
-        // Now, print a bar in the graph.
         cout << i << ": ";
         for (int k = 0; k < count; ++k) cout << "|"; // j was OK, but this is easy to read
         cout << endl;
@@ -69,11 +49,17 @@ void barGraph(vector<int> v) {
 }
 
 void stats(vector<int> v) {
+  
     int sum = 0;
+   
     int mode = v.at(0); // Already handled empty vector error in barGraph()
+  
     int modeCount = 0;
+ 
     int modeValMax = 0;
+ 
     int modeCountMax = 0;
+   
     for (unsigned int i = 0; i < v.size(); ++i) {
         sum += v.at(i);
         if (v.at(i) == mode) modeCount++;
@@ -94,14 +80,19 @@ void stats(vector<int> v) {
 
 int main()
 {
+ 
     vector<int> dataVec;
+  
     int nextInt;
+
     char yesNo;
+ 
     do {
         cout << "Enter your data set of positive integers." << endl;
         cout << "Enter -1 as the final number to indicate the end of the data set." << endl;
         
         // Take data from user input
+  
         do {
             cin >> nextInt;
             if (nextInt >= 0 && nextInt <= 200) dataVec.push_back(nextInt);
@@ -111,8 +102,11 @@ int main()
         } while (nextInt != -1);
         // Call functions to process data
         if (dataVec.size() > 0) sort(dataVec.begin(), dataVec.end());   // Makes stats functions easier.
+     
         barGraph(dataVec);
+   
         stats(dataVec);
+     
         dataVec.clear();
         
         cout << "Would you like to do another? (Enter Y for yes)" << endl;
