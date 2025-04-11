264837d7b04d69be8f213e8142e1ffdc321c54ec
runtimeError4004 <128573743+runtimeError4004@users.noreply.github.com>
2025-04-10 17:16:36 -0700

    refactoring
    Made changes to variable names, common practices for array data handling, and removing unneeded comments

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@ Coding HW: Bar Graphs (Vectors)
 This is a program that performs basic statistical analysis on sets of user data
 stored as vectors. I'm coding these bar graphs on a bar stool.
 
-(Don't look at me like that. I work here.)
-
 Author: Benjamin DeVore
 Date: 1 November 2024
 
@@ -18,8 +16,8 @@ using namespace std;
 
 // Vector printing function.  Provided for convenience for intermediate testing.
 void printVector(vector<int> v){
-    for (int e: v){
-        cout << e << " ";
+    for (int number: row){
+        cout << number << " ";
     }
     cout << endl;
 }
@@ -46,49 +44,47 @@ void sortVec(vector<int> &v) {  // Pass by reference to sort the input vector
     }
 } */
 
-void barGraph(vector<int> v) {
+void barGraph(vector<int> arr) {
     int count = 0;
-    int currNum = v.at(0);
-    if (v.size() == 0) {
+    int currNum = arr.at(0);
+    if (arr.size() == 0) {
         // Handle an edge case.
         cout << "ERROR: Empty vector! Aborting.\n";
         return;
     }
-    for (unsigned int i = v.at(0); i <= v.at(v.size() - 1); ++i) {
-        for (int j : v) if (j == i) count++;
-        // Brevity is the soul of wit.
-        // (AKA *I* know what this is doing...)
-        
+    for (unsigned int i = arr.at(0); i <= arr.at(arr.size() - 1); ++i) {
+        for (int j: arr) {
+            if (j == i) count++;
+        }
         // Now, print a bar in the graph.
         cout << i << ": ";
         for (int k = 0; k < count; ++k) cout << "|"; // j was OK, but this is easy to read
         cout << endl;
         count = 0;
     }
-    // God, this went so much better than my first attempt at this function.
 }
 
-void stats(vector<int> v) {
+void stats(vector<int> arr) {
     int sum = 0;
-    int mode = v.at(0); // Already handled empty vector error in barGraph()
+    int mode = arr[0]; 
     int modeCount = 0;
     int modeValMax = 0;
     int modeCountMax = 0;
-    for (unsigned int i = 0; i < v.size(); ++i) {
-        sum += v.at(i);
-        if (v.at(i) == mode) modeCount++;
+    for (unsigned int i = 0; i < arr.size(); ++i) {
+        sum += arr[i];
+        if (arr[i] == mode) modeCount++;
         else {
             if (modeCount >= modeCountMax) {
-                if (i != v.size() - 1) modeValMax = v.at(i - 1);
-                else modeValMax = v.at(i);
+                if (i != v.size() - 1) modeValMax = v[i - 1];
+                else modeValMax = v[i];
                 modeCountMax = modeCount;
             }
-            mode = v.at(i);
+            mode = v[i];
             modeCount = 1;
         }
     }
     printf("Mean: %.2f\n", (double) sum / v.size());
-    printf("Median: %d\n", v.at(v.size() / 2));
+    printf("Median: %d\n", v[v.size() / 2);
     printf("Mode: %d\n", modeValMax);
 }
 
@@ -96,7 +92,8 @@ int main()
 {
     vector<int> dataVec;
     int nextInt;
-    char yesNo;
+    char response;
+    bool pass = false;
     do {
         cout << "Enter your data set of positive integers." << endl;
         cout << "Enter -1 as the final number to indicate the end of the data set." << endl;
@@ -117,7 +114,8 @@ int main()
         
         cout << "Would you like to do another? (Enter Y for yes)" << endl;
         cin >> yesNo;
-    } while(yesNo == 'Y' || yesNo == 'y');
+        if (response == 'Y' || response == 'y') pass = true;
+    } while(pass);
     
     return 0;
 }
