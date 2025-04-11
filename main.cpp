cb737cdd4a5b4a0b87a37ea16b0f9dc1ad31d122
Albert Castillo <Albert.castillom@gmail.com>
2025-04-10 17:11:13 -0700

    1st Commit removed some unused comments and notes also change barGraph to printBarGraph

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ Date: 1 November 2024
 #include <iostream>
 #include <cctype>
 #include <vector>
-#include <algorithm> // Sorry. Had to use <algorithm>'s sort() method.
+#include <algorithm>
 using namespace std;
 
 // Vector printing function.  Provided for convenience for intermediate testing.
@@ -24,29 +24,8 @@ void printVector(vector<int> v){
     cout << endl;
 }
 
-/* NOTE: I tried to write my own vector sorting function but I just couldn't get
-it to work. I don't quite understand what I was reading about how I have to set
-iterator types using a pointer. I'm going to admit defeat and use <algorithm>
-to sort this vector. I would love some feedback on what I'm doing wrong here.
 
-// Vector sorting function. "Makin' Medians EZ" since 2024.
-void sortVec(vector<int> &v) {  // Pass by reference to sort the input vector
-    int min;
-    auto minpos = v.begin();
-    for (int i = 0; i < v.size(); ++i) {
-        min = 201;
-        for (int j = i; j < v.size(); ++j) {
-            if (v.at(j) <= min) {
-                min = v.at(j);
-                *minpos = j;
-            }
-        }
-        v.erase(minpos);
-        v.insert(v.begin(), min);
-    }
-} */
-
-void barGraph(vector<int> v) {
+void PrintBarGraph(vector<int> v) {
     int count = 0;
     int currNum = v.at(0);
     if (v.size() == 0) {
@@ -56,10 +35,9 @@ void barGraph(vector<int> v) {
     }
     for (unsigned int i = v.at(0); i <= v.at(v.size() - 1); ++i) {
         for (int j : v) if (j == i) count++;
-        // Brevity is the soul of wit.
-        // (AKA *I* know what this is doing...)
-        
-        // Now, print a bar in the graph.
+
+
+        // print a bar in the graph.
         cout << i << ": ";
         for (int k = 0; k < count; ++k) cout << "|"; // j was OK, but this is easy to read
         cout << endl;
@@ -111,7 +89,7 @@ int main()
         } while (nextInt != -1);
         // Call functions to process data
         if (dataVec.size() > 0) sort(dataVec.begin(), dataVec.end());   // Makes stats functions easier.
-        barGraph(dataVec);
+        PrintBarGraph(dataVec);
         stats(dataVec);
         dataVec.clear();
         
